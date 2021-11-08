b2090887476c869423209522ed35b8edbc1898ff
Lennart van Laake <lennartvanlaake@gmail.com>
2021-11-08 08:45:34 +0100

    work on command thing

--- packages/command/src/text.ts
+++ packages/command/src/text.ts
@@ -1,13 +1,24 @@
+/// <reference types="@legalthingy/shared"/>
+
 import Router from 'koa-router';
+import { TextMessage } from 'packages/shared/src/shared.js';
+import { producer } from './kafka.js';
+import { assertType } from 'typescript-is';
 
 export const textRouter = new Router();
 
 textRouter.post('/', async (ctx, next) => {
+	assertType<TextMessage>(ctx.request.body);
+	const requestBody: TextMessage = ctx.request.body;
+	producer.send({
+		topic: 'text',
+		messages: [{ value: requestBody.message }],
+	});
 	ctx.body = 'bla';
 	await next();
 });
 
 textRouter.get('/', async (ctx, next) => {
-	ctx.body = 'bla';
+	ctx.body = 'Thanks mate!';
 	await next();
 });
