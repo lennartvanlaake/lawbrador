b8ac06054ac7fff8b3ad5ce9afaae169cdb8d477
Lennart van Laake <lennartvanlaake@gmail.com>
2021-11-19 10:57:55 +0100

    added integration test

--- packages/tests/cypress/integration/spec.ts
+++ packages/tests/cypress/integration/spec.ts
@@ -1,9 +1,11 @@
 describe('Test if it creates a new page', () => {
 	it('Access index page', () => {
+		const TEXT = 'doekmans';
 		cy.visit('/');
-		cy.get('#text-field').type('doekmans');
-		cy.get('#text-field').type('torrrrie');
-		cy.get('#text-field').type('haaaat');
-		cy.get('button').click();
+		cy.get('#text-field').type(TEXT);
+		cy.get('button').trigger('click');
+		cy.contains(TEXT).click();
+		cy.url().should('include', 'text');
+		cy.get('p').should('contain.text', TEXT);
 	});
 });
