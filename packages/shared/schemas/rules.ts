872efc29baa9e35c9f91b045f79052504b40d846
Lennart van Laake <lennartvanlaake@gmail.com>
2021-11-29 10:56:22 +0100

    added schema rulesets

---
+++ packages/shared/schemas/rules.ts
@@ -0,0 +1,45 @@
+import { Static, Type } from '@sinclair/typebox';
+
+export enum SelectionOperator {
+	Is,
+	Includes,
+}
+
+export const selectionOperator = Type.Enum(SelectionOperator, {
+	$id: 'selectionOperator',
+});
+
+export enum SelectionLocation {
+	Tag,
+	Class,
+	Id,
+}
+
+export const selectionLocation = Type.Enum(SelectionLocation, {
+	$id: 'selectionLocation',
+});
+
+export const selectionRule = Type.Object(
+	{
+		op: Type.Ref(selectionOperator),
+		location: Type.Ref(selectionLocation),
+		value: Type.String(),
+	},
+	{ $id: 'selectionRule' },
+);
+
+export const urlSelectionRule = Type.Object(
+	{
+		op: Type.Ref(selectionOperator),
+		value: Type.String(),
+	},
+	{ $id: 'urlSelectionRule' },
+);
+
+export type SelectionRule = Static<typeof selectionRule>;
+export type UrlSelectionRule = Static<typeof urlSelectionRule>;
+
+export const RuleSet = Type.Object({
+	urlRules: Type.Ref(urlSelectionRule),
+	bodyRule: Type.Optional(selectionRule),
+});
