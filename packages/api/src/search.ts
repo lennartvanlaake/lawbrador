3397d7cd7cd079f616e61394965948dd08e98783
Lennart van Laake <lennartvanlaake@gmail.com>
2021-12-15 12:30:43 +0100

    basic cacheing of searches seems to work

--- packages/api/src/search.ts
+++ packages/api/src/search.ts
@@ -1,13 +1,32 @@
 import type { FastifyPluginAsync } from 'fastify';
 import { search } from '@legalthingy/parse/src/searcher';
+import { hashObject } from '@legalthingy/parse/src/url';
 import { SearchRequest } from '@legalthingy/shared/schemas/search';
 import { getSourceConfigById } from './source_configs';
 import { getCollection } from './utils';
+
 export const searchRoutes: FastifyPluginAsync = async (fastify) => {
 	const documentCollection = getCollection(fastify, 'documents');
+	const searchColleciton = getCollection(fastify, 'search');
 	fastify.post<{ Body: SearchRequest }>('/api/search', async (req) => {
 		const config = getSourceConfigById(req.body.sourceConfigId);
-		const results = await search(req.body.searchParams, config);
+		const queryParamsHash = hashObject({
+			params: req.body.searchParams,
+			config: config,
+		});
+		const cachedResults = await searchColleciton.findOne({
+			hash: queryParamsHash,
+		});
+		let results;
+		if (cachedResults) {
+			results = cachedResults.list;
+		} else {
+			results = await search(req.body.searchParams, config);
+			searchColleciton.insertOne({
+				hash: queryParamsHash,
+				list: results,
+			});
+		}
 		const resultHashes = results.map((r) => r.hash);
 		const documents = await documentCollection
 			.find({
