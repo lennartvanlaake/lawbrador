8bda0ac26d917d1e4edf2ec72074552bea53ac85
Lennart van Laake <lennartvanlaake@gmail.com>
2022-01-27 09:24:41 +0100

    use bitnami docker image with simple replica set config

--- packages/shared/src/db/constants.ts
+++ packages/shared/src/db/constants.ts
@@ -1,5 +1,8 @@
 import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import path from 'path'
 
+dotenv.config({ path: path.join('../..','.env') });
 export const DB_NAME = "lawbrador";
-export const CONNECTION_STRING = "mongodb://localhost:27017?replicaSet=rs0&readPreference=primary&ssl=false";
+export const CONNECTION_STRING = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
 export const LAWBRADOR_CLIENT = new MongoClient(CONNECTION_STRING);
