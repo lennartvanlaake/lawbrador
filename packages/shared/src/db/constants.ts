a118f60545eeb35c62824a0a3fcbdf5ef27ae1d9
Lennart van Laake <lennartvanlaake@gmail.com>
2022-01-29 15:43:57 +0100

    generic message

--- packages/shared/src/db/constants.ts
+++ packages/shared/src/db/constants.ts
@@ -4,6 +4,6 @@ import path from 'path'
 
 dotenv.config({ path: path.join('../..','.env') });
 export const DB_NAME = "lawbrador";
-export const CONNECTION_STRING = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_ROOT_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+export const CONNECTION_STRING = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
 console.log(CONNECTION_STRING);
 export const LAWBRADOR_CLIENT = new MongoClient(CONNECTION_STRING);
