cd536de065d054471fd5fd83a419ba2c7c36ab4b
Lennart van Laake <lennartvanlaake@gmail.com>
2022-02-22 14:55:39 +0000

    remove dist dir

--- packages/api/dist/clear.js
+++
@@ -1,9 +0,0 @@
-import { Endpoints } from '@lawbrador/shared';
-import { clean } from '@lawbrador/db';
-export default async (fastify) => {
-    fastify.delete(Endpoints.ALL, {}, async () => {
-        await clean(fastify.client);
-        return { status: 'success' };
-    });
-};
-//# sourceMappingURL=clear.js.map
