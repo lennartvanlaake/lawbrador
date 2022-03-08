b3dc21dd14022b324163d15a534afad5a58f3345
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-08 10:32:08 +0000

    wip tooltip button

---
+++ packages/fe/src/lib/components/common/TooltipButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let showTooltip = false;
+	export let tooltipText:string;
+</script>
+
+<i class="fas fa-circle-question" class:active={showTooltip}></i>
+
+{#if showTooltip }
+<p>{ tooltipText }</p>
+{/if}
+
+<style>
+	i {
+		color: var(--accent);
+	}
+	.active {
+		color: white;
+		background-color: var(--accent);
+	}
+</style>
