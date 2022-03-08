4171e6e5159e673fab7f67199272bcf67651aa34
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-08 08:24:14 +0000

    add readme

--- packages/fe/src/routes/__layout.svelte
+++ packages/fe/src/routes/__layout.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
 </script>
-
+<nav>
+	<a class="left" href="/">Home</a>
+	<a class="right" href="/about">About</a>
+</nav>
+<div id="spacer"></div>
 <slot></slot>
 
 <style>
 	:global(:root) {
-		/*lawbrador theme yellow*/
-		/*--bg:#fff7d5;*/
-		/*lawbrador theme brown*/
 		--accent:#815307;
 	}
 	:global(section) {
 		margin-left: 2rem;
+	}
+	#spacer {
+		margin-bottom: 6rem;
+	}
+	.left {
+		float: left;
+	}
+
+	.right {
+		float: right;
+	}
+
+	nav {
+		max-width: 45rem;
+		width: 100%;
+		position: fixed;
+		background-color: white;
 	}
 
 </style>
