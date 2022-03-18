99185c0752c0a053a967644c308297452b0df5ad
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-18 15:27:24 +0000

    added tests

--- packages/shared/src/wrapHtmlElements/test-warp.spec.ts
+++ packages/shared/src/wrapHtmlElements/test-warp.spec.ts
@@ -14,4 +14,14 @@ describe("wrapping search terms in html elements", () => {
     const result = wrapElements(html, "bla iets", start, end);
     expect(result).to.eq(`<p>${start}bla${end} die ${start}iets${end}</p>`);
   });
+  it("wraps match containig element", () => {
+    const html = "<p>b<i>l</i>a</p>";
+    const result = wrapElements(html, "bla iets", start, end);
+    expect(result).to.eq(`<p>${start}b<i>l</i>a${end}</p>`);
+  });
+  it("wraps multi-word term in one element", () => {
+    const html = "<p>bla iets</p>";
+    const result = wrapElements(html, "bla iets", start, end);
+    expect(result).to.eq(`<p>${start}bla iets${end}</p>`);
+  });
 });
