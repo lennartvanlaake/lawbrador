ad5e61f1008941e6e80f7236244550ec7673d6a9
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-23 08:41:17 +0000

    make hashtag links scroll to center

--- packages/fe/src/lib/components/view/NodeView.svelte
+++ packages/fe/src/lib/components/view/NodeView.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
+	import { onMount, tick } from 'svelte';
+	import { browser } from '$app/env';
 	export let html: string;
+	const hashtagRegex = /#.*/;
+	onMount(async () => {
+		await makeHashtagLinksScrollToCenter();
+	});
+
+	async function makeHashtagLinksScrollToCenter() {
+		if (!browser) return;
+		await tick();
+		const links = Array.from(document.getElementsByTagName('a'));
+		links.forEach((it) => {
+			const targetId = (it as HTMLAnchorElement).href.match(hashtagRegex);
+			if (targetId) {
+				// zero-index gets matched string, substring(1) removes '#'
+				makeScrollToCenter(it, targetId[0].substring(1));
+			}
+		});
+	}
+
+	function makeScrollToCenter(linkElement: HTMLAnchorElement, elementId: string) {
+		linkElement.addEventListener('click', (ev) => {
+			if (!elementId) return;
+			const targetElement = document.getElementById(elementId);
+			if (!targetElement) return;
+			ev.preventDefault();
+			targetElement.scrollIntoView({ block: 'center' });
+		});
+	}
 </script>
 
 {@html html}
