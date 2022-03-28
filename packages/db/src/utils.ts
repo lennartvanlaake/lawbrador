d4165da16f775ca7f90067f2abe3abbffe48e44f
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-28 17:56:19 +0000

    drop db for clean command

--- packages/db/src/utils.ts
+++ packages/db/src/utils.ts
@@ -7,8 +7,5 @@ export function id() {
 }
 
 export async function clean(client: MongoClient) {
-  const collections = await client.db(DB_NAME).collections();
-  collections.forEach(async (c) => {
-    await c.deleteMany({});
-  });
+  await client.db(DB_NAME).dropDatabase();
 }
