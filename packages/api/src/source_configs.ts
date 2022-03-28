c63e9ef9029e7dfa2553ae79b191fd0b4bacd26a
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-28 15:33:34 +0000

    remove schema for getting source configS

--- packages/api/src/source_configs.ts
+++ packages/api/src/source_configs.ts
@@ -2,19 +2,14 @@ import type { FastifyInstance } from "fastify";
 import type { SourceSiteConfig } from "@lawbrador/shared";
 import { Endpoints, Schemas, toIdentity } from "@lawbrador/shared";
 import { id } from "@lawbrador/db";
-import { routeConfig } from "./utils";
 import { EventFactory } from "@lawbrador/events";
 import { auth } from "./auth";
 
 export default async (fastify: FastifyInstance) => {
-  fastify.get(
-    Endpoints.SOURCES,
-    routeConfig(Schemas.sourceSiteConfigs),
-    async () => {
-      const sourceConfigs = await fastify.collections.sourceConfigs.all();
-      return sourceConfigs;
-    }
-  );
+  fastify.get(Endpoints.SOURCES, async () => {
+    const sourceConfigs = await fastify.collections.sourceConfigs.all();
+    return sourceConfigs;
+  });
   fastify.route<{ Body: Omit<SourceSiteConfig, "_id"> }>({
     method: "POST",
     url: Endpoints.SOURCES,
