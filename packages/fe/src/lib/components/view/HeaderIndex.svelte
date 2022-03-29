141a154aaca603c721769c56e1297f15e44d79d8
Lennart van Laake <lennartvanlaake@gmail.com>
2022-03-29 12:48:51 +0000

    nested structure for index

--- packages/fe/src/lib/components/view/HeaderIndex.svelte
+++ packages/fe/src/lib/components/view/HeaderIndex.svelte
@@ -2,10 +2,62 @@
 	import { scrollToCenter } from '$lib/ts/utils';
 
 	import type { RestructuredNode } from '@lawbrador/shared';
+	import { renderText } from '@lawbrador/shared';
 	import { renderNode } from '@lawbrador/shared';
 	import Modal from '../common/Modal.svelte';
 	export let show = false;
-	export let headers: RestructuredNode[];
+	export let headers: RestructuredNode[] = [];
+	interface Header {
+		parent: Header | null;
+		fullText: string;
+		level: number;
+		children: Header[];
+	}
+	$: nestedHeaders = nest(headers);
+
+	function toHeader(node: RestructuredNode): Header {
+		return { fullText: renderText(node), level: getHeaderLevel(node), children: [], parent: null };
+	}
+
+	function getFirstLowerHeader(parentHeader: Header | null, currentLevel: number) {
+		if (!parentHeader) return null;
+		if (parentHeader.level < currentLevel) return parentHeader;
+		return getFirstLowerHeader(parentHeader.parent, currentLevel);
+	}
+
+	function nest(headerList: RestructuredNode[]) {
+		const headers = headerList.map((it) => toHeader(it));
+		let previousHeader: Header | null = null;
+		for (let i = 0; i < headers.length; i++) {
+			const currentHeader = headers[i];
+			if (!currentHeader) break;
+			currentHeader.parent = previousHeader;
+			const firstLowerHeader = getFirstLowerHeader(previousHeader, currentHeader.level);
+			if (previousHeader && previousHeader.level < currentHeader.level) {
+				previousHeader.children.push(currentHeader);
+			} else if (previousHeader && previousHeader.level == currentHeader.level) {
+				previousHeader.parent?.children.push(currentHeader);
+			} else if (firstLowerHeader) {
+				firstLowerHeader.children.push(currentHeader);
+			} else {
+				previousHeader = currentHeader;
+				continue;
+			}
+			previousHeader = currentHeader;
+			headers.splice(i, 1);
+			i--;
+		}
+		console.log(headers);
+		return headers;
+	}
+
+	function getHeaderLevel(node: RestructuredNode) {
+		try {
+			return parseInt(node.name.charAt(1));
+		} catch (e) {
+			return Number.MAX_VALUE;
+		}
+	}
 
 	function closeAndScroll(ev: Event, header: RestructuredNode) {
 		ev.preventDefault();
--- packages/fe/src/routes/workbench/view/HeaderIndex.svelte
+++ packages/fe/src/routes/workbench/view/HeaderIndex.svelte
@@ -2,7 +2,10 @@
 	import View from '$lib/components/workbench/View.svelte';
 	import { defaultRestructure } from '@lawbrador/shared';
 	import HeaderIndex from '$lib/components/view/HeaderIndex.svelte';
-	let headers = defaultRestructure('<h1>1 bla</h1><h2>2 die</h2><h1>3 bla</h1>').children ?? [];
+	let headers =
+		defaultRestructure(
+			'<h1>1 bla</h1><h2>1.1 die</h2><h3>1.1.1 nogiets</h3><h3>1.1.2 dingen</h3><h2>1.2 iets</h2><h1>3 bla</h1>'
+		).children ?? [];
 	console.log(headers);
 </script>
 
