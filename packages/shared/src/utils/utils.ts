f554d275bbbee7dc66ac3d7f668033a3cb1592b0
Your Name <you@example.com>
2022-05-08 16:17:23 +0200

    try to fix uuid method with copy-pasta

--- packages/shared/src/utils/utils.ts
+++ packages/shared/src/utils/utils.ts
@@ -4,9 +4,22 @@ import { eurlexConfig } from "..";
 import { parse } from "../parse/scraper";
 import { applyRuleSet } from "../restructure";
 
+// shameless copy-pasta from stack overflow
 export function id() {
-  return crypto.randomUUID(); 
-}
+  var d = new Date().getTime();//Timestamp
+  var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16;//random number between 0 and 16
+      if(d > 0){//Use timestamp until depleted
+          r = (d + r)%16 | 0;
+          d = Math.floor(d/16);
+      } else {//Use microseconds since page-load if supported
+          r = (d2 + r)%16 | 0;
+          d2 = Math.floor(d2/16);
+      }
+      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  })
+;}
 
 export function last(array: any[]) {
   return array[array.length - 1];
